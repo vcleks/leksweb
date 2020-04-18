74813edc96bb6d350eb7e40a11d50d1c89c74a9b
Leks <vcleks28@gmail.com>
2020-04-18 16:33:22 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const { src, dest, parallel, watch, series } = require('gulp');
+const fileinclude = require('gulp-file-include');
+const del = require('del');
+
+
+function clean() {
+    return del('output/');
+}
+
+function html() {
+    return src('pages/*.html')
+        .pipe(fileinclude({
+            basepath: 'includes/',
+        }))
+        .pipe(dest('output/'));
+}
+
+function staticContent() {
+    return src('static/**')
+        .pipe(dest('output/'));
+}
+
+exports.default = series(clean, function() {
+    watch(['pages/*.html', 'includes/'], { ignoreInitial: false }, html);
+    watch(['static/'], { ignoreInitial: false }, staticContent);
+});
+
+exports.build = series(clean, parallel(html, staticContent));
+
+exports.clean = clean;
